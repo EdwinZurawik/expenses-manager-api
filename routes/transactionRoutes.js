78a35d12f244c5e5d3cee297da7a74599d5f5251
Edwin Żurawik <ezurawik35@gmail.com>
2022-01-22 13:57:58 +0100

    feat: middleware for checking id
    - moved Id checking to a separate middleware
    - added checkBody middleware

--- routes/transactionRoutes.js
+++ routes/transactionRoutes.js
@@ -3,10 +3,15 @@ const transactionController = require('../controllers/transactionController');
 
 const router = express.Router();
 
+router.param('id', transactionController.checkId);
+
 router
   .route('/')
   .get(transactionController.getAllTransactions)
-  .post(transactionController.createTransaction);
+  .post(
+    transactionController.checkBody,
+    transactionController.createTransaction
+  );
 router
   .route('/:id')
   .get(transactionController.getTransaction)
