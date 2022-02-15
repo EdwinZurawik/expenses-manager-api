d9109a93e5f43136f508c5582a5d362aebfed14b
Edwin Żurawik <ezurawik35@gmail.com>
2022-02-15 22:36:27 +0100

    feat: added jwt token on signup
    - using jsonwebtoken library
    - generate jwt token on signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,22 @@
+const jwt = require('jsonwebtoken');
 const Account = require('../models/accountModel');
 const catchAsync = require('../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newAccount = await Account.create(req.body);
+  const newAccount = await Account.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = jwt.sign({ id: newAccount._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       account: newAccount,
     },
